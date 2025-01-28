2e86d4259860116cf9f01a95134962b74ea8bc70
野うさぎ <123544007+pekora-tech@users.noreply.github.com>
2025-01-28 23:04:16 +0800

    Modify

--- llama-3.2-webgpu/src/main.js
+++ llama-3.2-webgpu/src/main.js
@@ -13,7 +13,7 @@ let viteProcess = null;
 const createWindow = () => {
   mainWindow = new BrowserWindow({
     width: 800,
-    height: 600,
+    height: 480,
     webPreferences: {
       nodeIntegration: true,
       contextIsolation: false
